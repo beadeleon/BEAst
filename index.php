285209fb2e59a8a9996ba074ff6b772b1f9091ba
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:40:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Assignment 05</title>
 <body>
 
-<center><h1>DoquilaF Photo Gallery </h1></center>
+<center><h1>DoquilaQ.A Photo Gallery </h1></center>
 <center><p>List of Photo </p></center>
 <center><p><a href="photo1.php">Photo 1</p></center>
 <center><p><a href="photo2.php">Photo 2</p></center>
