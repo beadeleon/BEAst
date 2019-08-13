a88b08d095f46ea8e571ea0e6cfb313c7ee39344
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:49:14 +0800

    Update photo2.php

--- photo2.php
+++ photo2.php
@@ -7,6 +7,6 @@
 <img src="https://asset-sports.abs-cbn.com/web/dev/videos/1524133138_videos-422x300-bdl2.jpg">
 <h2><nav>
 <a href="index.php">Back to Home</a>
-<a href="photo3.php.html">Next Photo</a>
+<a href="photo3.php">Next Photo</a>
 </body>
 </html>
