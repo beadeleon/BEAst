2f5e1d377a86ca050506de7f9a8666b045853ba1
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:41:13 +0800

    Update photo2.php

--- photo2.php
+++ photo2.php
@@ -3,7 +3,7 @@
 <title>Pic</title>
 <body>
 <center>
-<h1>DoquilaF Photo Gallery</h1>
+<h1>DoquilaQ.A Photo Gallery</h1>
 <img src="https://asset-sports.abs-cbn.com/web/dev/videos/1524133138_videos-422x300-bdl2.jpg">
 <h2><nav>
 <a href="index.php">Back to Home</a>
