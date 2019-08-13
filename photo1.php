ec4e87d61197afffbffbc24691490c70c1bd3e56
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 09:43:53 +0800

    Create photo1.php

---
+++ photo1.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<title>Pic</title>
+<body>
+
+<h1>DoquilaF Photo Gallery</h1>
+<img src="https://scontent.fceb2-2.fna.fbcdn.net/v/t1.0-9/48394758_2013312355633401_6763171369061449728_n.jpg?_nc_cat=107&_nc_oc=AQknQYDeqvBiU9okNCB09UCXOeptOGB5RJw5ptHaTsMPqx8HtOKDCsLi36Bb117E6OQ&_nc_ht=scontent.fceb2-2.fna&oh=7bc54839e4ff86e59ed5434a41cd71d3&oe=5DD10689">
+<a href="https://">This is a link</a>
