ef0022b13a041181656586c69bf057bb6b11dcda
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:22:00 +0800

    Create photo2.php

---
+++ photo2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<title>Pic</title>
+<body>
+<center>
+<h1>DoquilaF Photo Gallery</h1>
+<img src="https://asset-sports.abs-cbn.com/web/dev/videos/1524133138_videos-422x300-bdl2.jpg">
+<h2><nav>
+<a href="photo1">Back to Home</a>
+<a href="photo3.php.html">Next Photo</a>
+</body>
+</html>
