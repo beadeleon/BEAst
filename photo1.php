f34a7dd9bbed8bf07d1946e0772f2b4675b7960f
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:29:30 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -6,5 +6,5 @@
 <h1>DoquilaF Photo Gallery</h1>
 <img src="https://scontent.fceb2-2.fna.fbcdn.net/v/t1.0-9/48394758_2013312355633401_6763171369061449728_n.jpg?_nc_cat=107&_nc_oc=AQknQYDeqvBiU9okNCB09UCXOeptOGB5RJw5ptHaTsMPqx8HtOKDCsLi36Bb117E6OQ&_nc_ht=scontent.fceb2-2.fna&oh=7bc54839e4ff86e59ed5434a41cd71d3&oe=5DD10689">
 <h2><nav>
-<a href="home page.html">Back to Home</a>
+<a href="index.php">Back to Home</a>
 <a href="photo2.php.html">Next Photo</a>
