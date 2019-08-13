90314a44f0de7764b9d48d1f2131956af6f5da29
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:40:01 +0800

    Update photo3.php

--- photo3.php
+++ photo3.php
@@ -3,7 +3,7 @@
 <title>Pic</title>
 <body>
 <center>
-<h1>DoquilaF Photo Gallery</h1>
+<h1>DoquilaQ.A Photo Gallery</h1>
 <img src=https://pbs.twimg.com/profile_images/962612366939582464/cj4eJ27t_400x400.jpg
 <h2><nav>
 <a href="index.php">Back to Home</a>
