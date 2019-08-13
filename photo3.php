f3b6b4bf4ee049b9975ccd9a57f6861a96d797cb
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 10:26:30 +0800

    Create photo3.php

---
+++ photo3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<title>Pic</title>
+<body>
+<center>
+<h1>DoquilaF Photo Gallery</h1>
+<img src=https://pbs.twimg.com/profile_images/962612366939582464/cj4eJ27t_400x400.jpg
+<h2><nav>
+<a href="home.html">Back to Home</a>
+
+</body>
+</html>
