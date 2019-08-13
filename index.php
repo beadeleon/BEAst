e4977369850b1fde6360aa716d879873371f0956
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-13 09:35:59 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<title>Assignment 05</title>
+<body>
+
+<h1>DoquilaF Photo Gallery </h1>
+<p>List of Photo </p>
+<p>Photo 1 </p>
+<p>Photo 2 </p>
+<p> Photo 3</P>
+
+</body>
+</html>
