c8a1317c2669c90709fc5a8e1667fbd77df4104b
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-08-15 10:11:12 +0800

    Update photo3.php

--- photo3.php
+++ photo3.php
@@ -7,6 +7,9 @@
 <img src=https://pbs.twimg.com/profile_images/962612366939582464/cj4eJ27t_400x400.jpg
 <h2><nav>
 <a href="index.php">Back to Home</a>
+  <nav>
+    <a href="login.html">log out</a>
+  </nav>
 
 </body>
 </html>
