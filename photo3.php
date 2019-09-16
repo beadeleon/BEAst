d61400318deefb9c82f81f89e1e42967e9c5ffc2
beadeleon <53499992+beadeleon@users.noreply.github.com>
2019-09-16 18:47:16 +0700

    Update photo3.php

--- photo3.php
+++ photo3.php
@@ -2,14 +2,17 @@
 <html>
 <title>Pic</title>
 <body>
-<center>
-<h1>DoquilaQ.A Photo Gallery</h1>
+<center><h2 style="font-family:calibre; color: white ;"DoquilaQ.A Photo Gallery</h2>
+<style>
+body{ background-color:red-black;}
+</style>
+
 <img src=https://pbs.twimg.com/profile_images/962612366939582464/cj4eJ27t_400x400.jpg
 <h2><nav>
 <a href="pg.html">Back to Home</a>
-  <nav>
-    <a href="index.php">log out</a>
-  </nav>
+  <nav>
+    <a href="index.php">log out</a>
+  </nav>
 
 </body>
 </html>
